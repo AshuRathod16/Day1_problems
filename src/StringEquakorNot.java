92c3f1fab94b88a6217f0c5fb01c38108e284b39
Ashwini Rathod <rashwini510@gmail.com>
2022-05-29 17:05:42 +0530

    Added:Check the string equal or not

---
+++ src/StringEquakorNot.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class StringEquakorNot {
+    public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter first String : ");
+            String firstName = scanner.nextLine();
+
+            System.out.print("Enter Second String : ");
+            String secondName = scanner.nextLine();
+
+            if(firstName.equals(secondName)) {
+                System.out.print("String is equal");
+            }
+            else
+            {
+                System.out.print("String is not equal");
+            }
+        }
+
+    }
+
