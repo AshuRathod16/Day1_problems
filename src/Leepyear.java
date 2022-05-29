ea426374249929b0ba0fb15a2a040cf47e938106
Ashwini Rathod <rashwini510@gmail.com>
2022-05-29 17:00:27 +0530

    Added:Leep year

---
+++ src/Leepyear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Leepyear {
+    public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter year :");
+            int year = scanner.nextInt();
+
+            if (year % 400 == 0 && year % 100 == 0 || year % 4 == 0)
+            {
+                System.out.println(year+ " is leap ");
+
+            }
+            else
+            {
+                System.out.println(year+ " is not a leap ");
+            }
+        }
+    }
+
